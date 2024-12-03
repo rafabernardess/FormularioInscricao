aa469e18abc5b8e1883d735db6e6fcf72f7a14c2
Rafaela Bernardes <160605081+rafabernardess@users.noreply.github.com>
2024-12-03 17:35:30 -0300

    Update FormularioPreenchido.jsx

--- src/components/FormularioPreenchido.jsx
+++ src/components/FormularioPreenchido.jsx
@@ -4,7 +4,7 @@ const FormularioPreenchido = () => {
   return (
     <div>
         <div className="logo">
-                <img src="../public/img/logo.png" alt="Logo" className="logo-img" />
+                <img src="/img/logo.png" alt="Logo" className="logo-img" />
             </div>
             <div className="content">
         <div className="fonte">
@@ -12,7 +12,7 @@ const FormularioPreenchido = () => {
             <h2>Preenchido!</h2>
         </div>
             <div className="icon">
-                <img src="public/img/icone.png" alt="Ícone" className="icon-img" />
+                <img src="/img/icone.png" alt="Ícone" className="icon-img" />
             </div>
             </div>
       
